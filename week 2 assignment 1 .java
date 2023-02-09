04904ba7cf1f1760f8ac157e8d7243a00ea6572e
HIMANSHU SHEKHAR <98458674+chulbul7061@users.noreply.github.com>
2023-02-09 10:50:42 +0530

    week 2 assignment 1.java

--- week 2 assignment 1 .java
+++ week 2 assignment 1 .java
@@ -8,7 +8,21 @@ class employeeInherit
   pubic input_user()
   {
     double bs,da,hra,ca,ea,mi,it,pf;
-   double gs,ns;
+    double gs,ns;
+
+    Scanner obj=new Scanner(System.in);
+
+    System.out.println("Enter Your Basic Salary");
+    System.out.println("Enter Your Dearness Allowance");
+    System.out.println("Enter Your House Rent Allowance");
+    System.out.println("Enter Your Conveyeance Allowance");
+    System.out.println("Enter Your Entertainment Allowance");
+    System.out.println("Enter Your Medical Insurance");
+    System.out.println("Enter Your Income Tax");
+    System.out.println("Enter Your Provident Fund");
+    System.out.println("Enter Your Gross Salary");
+    System.out.println("Your Net Salary");
+    
 
   }
 }
