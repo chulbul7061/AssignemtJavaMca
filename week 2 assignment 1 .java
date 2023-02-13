9806c1ab6700fa983b976788a438d3d6d98fa0b1
HIMANSHU SHEKHAR <98458674+chulbul7061@users.noreply.github.com>
2023-02-13 15:51:12 +0530

    Update week 2 assignment 1 .java

--- week 2 assignment 1 .java
+++ week 2 assignment 1 .java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 interface gross_salary
 {
-  public void gross_salary_Fun();
+    public void gross_salary_Fun();
 
 }
 
 interface Net_salary
 {
-  public void Net_salary_Fun();
+    public void Net_salary_Fun();
 }
 
 class your_net_salary implements gross_salary,Net_salary
 {
- 
+
 
     double basic,DA,HRA,TA=3600 ,TA_on_DA;
 
@@ -23,25 +23,25 @@ class your_net_salary implements gross_salary,Net_salary
 
     double Gross_salary;
     Scanner obj=new Scanner(System.in);
-  
+
     public void gross_salary_Fun()
     {
-   
 
-    
 
-    System.out.println("enter your Basic Salary");
-    basic=obj.nextDouble();
-    
-    System.out.println("enter your HRA Percantage 24 or 16 or 8 ");
-    
-    HRA=obj.nextDouble();
+
+
+        System.out.println("enter your Basic Salary");
+        basic=obj.nextDouble();
+
+        System.out.println("enter your HRA Percantage 24 or 16 or 8 ");
+
+        HRA=obj.nextDouble();
 
 
         DA=(basic*17)/100;
 
         HRA_cal=(basic*HRA)/100;
-        
+
         TA_on_DA=(TA*17)/100;
 
         Gross_salary=basic+DA+HRA_cal+TA+TA_on_DA;
@@ -52,15 +52,15 @@ class your_net_salary implements gross_salary,Net_salary
 
         System.out.println("=================================================================================================================");
     }
-    
-   ///////////////////////////////////////////NET_SALARY_/////////////////////////////////////////////////////////
 
- 
+    ///////////////////////////////////////////NET_SALARY_/////////////////////////////////////////////////////////
+
+
 
     double deduction,NPS,professional_tax=200,Medical=500;
     public void Net_salary_Fun()
     {
-         
+
         NPS=((basic+DA)*10)/100;
 
         deduction=Gross_salary-(NPS+professional_tax+Medical);
@@ -68,32 +68,33 @@ class your_net_salary implements gross_salary,Net_salary
         System.out.println("=================================================================================================================");
 
         System.out.println("NET_salary : "+deduction+" INR");
-    
-    
-
-       
-   }
-  
-   double PRAN_account;
-   /////////////////////////////////////////INNER  CLASS////////////////////////////////////////////////////////
-   class about_NPS
-   
-   {
-      public void inner_class_fun()
-      {
-        double employee_share=((basic+DA)*10)/100;
-        double government_share =((basic+DA)*14)/100;
-        PRAN_account=employee_share+government_share;
-
-        System.out.println("--------------------------------------------------------------------------------------------------------");
-        System.out.println(" About NPS amount : "+PRAN_account);
-        System.out.println("--------------------------------------------------------------------------------------------------------");
 
 
-      }
+
+
+    }
+
+    double PRAN_account;
+    /////////////////////////////////////////INNER  CLASS////////////////////////////////////////////////////////
+    class about_NPS
+
+    {
+        public void inner_class_fun()
+        {
+            double employee_share=((basic+DA)*10)/100;
+            double government_share =((basic+DA)*14)/100;
+            PRAN_account=employee_share+government_share;
+
+            System.out.println("--------------------------------------------------------------------------------------------------------");
+            System.out.println(" About NPS amount : "+PRAN_account);
+            System.out.println("--------------------------------------------------------------------------------------------------------");
+
+
+        }
     }
 
     double CTC;
+    double CTC2=0;
     void detailed_veiw()
     {
         System.out.println("Basic Salary= "+basic+" INR");
@@ -116,45 +117,33 @@ class your_net_salary implements gross_salary,Net_salary
 
         System.out.println("=================================================================================================================");
         System.out.println("Net Salary/ In Hand = "+deduction);
-        
+
 
         CTC = Gross_salary+PRAN_account;
         System.out.println("Your CTC = "+CTC*12 +" INR " +" Per annum");
-        
+        CTC2=CTC*12;
     }
-  //////////////////////////////////////////////tax calculate//////////////////////////  
-    if(CTC<=300000)
-    {
-      System.out.println("Your tax is Nill");
-      
+    //////////////////////////////////////////////tax calculate//////////////////////////
+  
+    void taxable() {
+        if (CTC2 <= 300000) {
+            System.out.println("Your tax is Nill");
+
+        } else if (CTC2 >= 300001 && CTC2 <= 600000) {
+            System.out.println("Your tax amount is 5% " + ((CTC2 * 5) / 100));
+        } else if (CTC2 >= 600001 && CTC2 <= 900000) {
+            System.out.println("Your tax amount is 10% " + ((CTC2 * 10) / 100));
+        } else if (CTC2 >= 900001 && CTC2 <= 1200000) {
+            System.out.println("Your tax amount is 15% " + ((CTC2 * 15) / 100));
+        } else if (CTC2 >= 1300001 && CTC2 <= 1500000) {
+            System.out.println("Your tax amount is 20% " + ((CTC2 * 20) / 100));
+        } else if (CTC2 >= 1500001) {
+            System.out.println("Your tax amount is 30% " + ((CTC2 * 30) / 100));
+        }
     }
-    else if(CTC>=300001 && CTC<=600000)
-    {
-      System.out.println("Your tax amount is 5% "+((CTC*5)/100));
-    }
-    
-     else if(CTC>=600001 && CTC<=900000)
-    {
-      System.out.println("Your tax amount is 10% "+((CTC*10)/100));
-    }
-    
-      else if(CTC>=900001 && CTC<=1200000)
-    {
-      System.out.println("Your tax amount is 15% "+((CTC*15)/100));
-    }
-    
-      else if(CTC>=1300001 && CTC<=1500000)
-    {
-      System.out.println("Your tax amount is 20% "+((CTC*20)/100));
-    }
-       else if(CTC>=1500001)
-    {
-      System.out.println("Your tax amount is 30% "+((CTC*30)/100));
-    }
-    
-    
-    
-    
+
+
+
 }
 
 
@@ -162,40 +151,46 @@ class your_net_salary implements gross_salary,Net_salary
 
 public class salary {
 
-     public static void main(String[] args)
-      {
+    public static void main(String[] args)
+    {
         double salary_slip;
         your_net_salary mainobj=new your_net_salary();
 
         mainobj.gross_salary_Fun();
         mainobj.Net_salary_Fun();
 
-        
 
-    your_net_salary.about_NPS inner_obj=mainobj.new about_NPS();
 
-    Scanner salary_slip_obj= new Scanner(System.in);
-    
-    
-    inner_obj.inner_class_fun();
+        your_net_salary.about_NPS inner_obj=mainobj.new about_NPS();
 
-    System.out.println("Press 1 to see salary slip ");
-    salary_slip=salary_slip_obj.nextDouble();
-    
+        Scanner salary_slip_obj= new Scanner(System.in);
+
+
+        inner_obj.inner_class_fun();
+
+        System.out.println("Press 1 to see salary slip or press to see taxable amount : ");
+        salary_slip=salary_slip_obj.nextDouble();
+
         if(salary_slip==1)
         {
-        mainobj.detailed_veiw();
+            mainobj.detailed_veiw();
+
         }
-        else 
+        else if (salary_slip==2)
+        {
+        mainobj.taxable();
+
+
+        } else
         {
             System.out.println("Invalid input ");
         }
 
 
 
-       
 
-        
+
+
     }
-    
+
 }
