62087c9f9341fd5ae90a60c3f63e088f461702c5
HIMANSHU SHEKHAR <98458674+chulbul7061@users.noreply.github.com>
2023-02-13 19:49:33 +0530

    Update week 2 assignment 2.java

--- week 2 assignment 2.java
+++ week 2 assignment 2.java
@@ -1,92 +1,111 @@
-import java.awt.desktop.SystemEventListener;
 import java.util.Scanner;
-
-
-class taxsaving
+class tax
 
 {
     double CTC2=0;
-    
+
+    Scanner input=new Scanner(System.in);
     void taxable()
     {
 
         System.out.println("enter your annual income ");
-        Scanner input=new Scanner(System.in);
+
         CTC2=input.nextDouble();
 
 
         if (CTC2 <= 300000)
         {
-        System.out.println("Your tax is Nill");
+            System.out.println("Your tax is Nill");
+
+
 
         } else if (CTC2 >= 300001 && CTC2 <= 600000) {
-        System.out.println("Your tax amount is 5% " + ((CTC2 * 5) / 100));
+            System.out.println("Your tax amount is 5% " + ((CTC2 * 5) / 100)+" INR");
+
         } else if (CTC2 >= 600001 && CTC2 <= 900000) {
-        System.out.println("Your tax amount is 10% " + ((CTC2 * 10) / 100));
+            System.out.println("Your tax amount is 10% " + ((CTC2 * 10) / 100)+" INR");
+
         } else if (CTC2 >= 900001 && CTC2 <= 1200000) {
-        System.out.println("Your tax amount is 15% " + ((CTC2 * 15) / 100));
+            System.out.println("Your tax amount is 15% " + ((CTC2 * 15) / 100)+" INR");
+
         } else if (CTC2 >= 1300001 && CTC2 <= 1500000) {
-        System.out.println("Your tax amount is 20% " + ((CTC2 * 20) / 100));
+            System.out.println("Your tax amount is 20% " + ((CTC2 * 20) / 100)+" INR");
+
         } else if (CTC2 >= 1500001) {
-        System.out.println("Your tax amount is 30% " + ((CTC2 * 30) / 100));
+            System.out.println("Your tax amount is 30% " + ((CTC2 * 30) / 100)+" INR");
+
         }
 
 
     }
 
     double result=0;
-    int ch;
-    System.out.println("enter your annual income ");
-    Scanner inputt=new Scanner(System.in);
-    ch=inputt.nextInt();
+
+
+
+
     void taxsavingScheam()
     {
-        switch (ch) 
+        System.out.println("Enter your choice for saving taxs ");
+        int ch=input.nextInt();
+
+        switch (ch)
         {
             case 1:
-            System.out.println("Term Life Insurance premium");
-            result = (CTC2 * 20) / 100;
-            System.out.println("Term Life Insurance premium" + result);
-            break;
-            
+                System.out.println("Term Life Insurance premium");
+                result +=15000;
+                System.out.println("Term Life Insurance premium " + result+" INR");
+                break;
+
             case 2:
-            System.out.println("PPF (Public Provident Fund)");
-            result = (CTC2 * 7.9) / 100;
-            System.out.println("(Public Provident Fund)" + result);
-            break;
-            
+                System.out.println("PPF (Public Provident Fund)");
+                result += 50000;
+                System.out.println("(Public Provident Fund)" + result+" INR");
+                break;
+
             case 3:
-            System.out.println("National Savings Certificates");
-            result = (CTC2 * 7.9) / 100;
-            System.out.println("National Savings Certificates" + result);
-            break;
-            
+                System.out.println("National Savings Certificates");
+                result += 40000;
+                System.out.println("National Savings Certificates " + result+" INR");
+                break;
+
             case 4:
-            System.out.println("Tuition fees:");
-            result = (CTC2 * 15) / 100;
-            System.out.println("Tuition fees:" + result);
-            break;
-            
+                System.out.println("Tuition fees:");
+                result += 96000;
+                System.out.println("Tuition fees:" + result+" INR");
+                break;
+
             case 5:
-            System.out.println("Sukanya Samriddhi Yojana (SSY):");
-            result = (CTC2 * 7.60) / 100;
-            System.out.println("Sukanya Samriddhi Yojana (SSY):" + result);
-            break;
-            
+                System.out.println("Sukanya Samriddhi Yojana (SSY):");
+                result += 50000;
+                System.out.println("Sukanya Samriddhi Yojana (SSY):" + result+" INR");
+                break;
+
             case 6:
-            System.out.println("Senior Citizen Saving Scheme (SCSS):");
-            result = (CTC2 * 7.40) / 100;
-            System.out.println("Senior Citizen Saving Scheme (SCSS):" + result);
-            break;
-            
+                System.out.println("Senior Citizen Saving Scheme (SCSS):");
+                result += 500000 ;
+                System.out.println("Senior Citizen Saving Scheme (SCSS):" + result+" INR");
+                break;
+
             case 7:
-            System.out.println("Home Loan:");
-            result = (CTC2 * 10) / 100;
-            System.out.println("Home Loan:" + result);
-            break;
-            
+                System.out.println("Home Loan:");
+                result += 60000;
+                System.out.println("Home Loan:" + result+" INR");
+                break;
+
             default:System.out.println("Invalid choice.....");
         }
+    }
+
+    void showscheme()
+    {
+        System.out.println(" press 1 Term Life Insurance premium");
+        System.out.println(" press 2 PPF (Public Provident Fund)");
+        System.out.println(" press 3 National Savings Certificates");
+        System.out.println(" press 4 Tuition fees:");
+        System.out.println(" press 5 Sukanya Samriddhi Yojana (SSY):");
+        System.out.println(" press 6 Senior Citizen Saving Scheme (SCSS):");
+        System.out.println(" press 7 Home Loan:");
     }
 
 
@@ -100,36 +119,11 @@ public class taxsaving{
 
     public static void main(String[] args)
     {
-        double salary_slip;
 
-
-        Scanner salary_slip_obj= new Scanner(System.in);
-
-
-        inner_obj.inner_class_fun();
-
-        System.out.println("Press 1 to see salary slip or 2 press to see taxable amount : ");
-        salary_slip=salary_slip_obj.nextDouble();
-
-        if(salary_slip==1)
-        {
-            mainobj.detailed_veiw();
-
-        }
-        else if (salary_slip==2)
-        {
-            mainobj.taxable();
-
-
-        } else
-        {
-            System.out.println("Invalid input ");
-        }
-
-
-
-
-
+        tax obj1=new tax();
+        obj1.taxable();
+        obj1.showscheme();
+        obj1.taxsavingScheam();
 
     }
 
